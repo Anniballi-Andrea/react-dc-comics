eae1bfe4bdd9dc7f0aa6f79c0e25c9dde7f0c432
Anniballi-Andrea <andreanniballi@gmail.com>
2025-11-04 18:36:28 +0100

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,64 @@
+export default function Footer() {
+
+
+
+    return (
+        <footer>
+            <div className="footer_bg">
+                <div className="container d-flex justify-content-between">
+                    <div className="d-flex">
+                        <div className="link">
+                            <ul>
+                                <li><h4>DC COMICS</h4></li>
+                                <li><a href="">Characters</a></li>
+                                <li><a href="">Comics</a></li>
+                                <li><a href="">Movies</a></li>
+                                <li><a href="">TV</a></li>
+                                <li><a href="">Games</a></li>
+                                <li><a href="">Videos</a></li>
+                                <li><a href="">Nevs</a></li>
+                                <li><h4>SHOP</h4></li>
+                                <li><a href="">Shop DC</a></li>
+                                <li><a href="">Shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
+                        <div className="link">
+                            <ul>
+                                <li><h4>DC</h4></li>
+                                <li><a href="">Terms of us</a></li>
+                                <li><a href="">Privacy policy (New)</a></li>
+                                <li><a href="">Ad Choices</a></li>
+                                <li><a href="">Advertising</a></li>
+                                <li><a href="">Jobs</a></li>
+                                <li><a href="">Subscriptions</a></li>
+                                <li><a href="">Talent Workshops</a></li>
+                                <li><a href="">CPSC Certificaties</a></li>
+                                <li><a href="">Ratings</a></li>
+                                <li><a href="">Shop Help</a></li>
+                                <li><a href="">Contact UsS</a></li>
+                            </ul>
+                        </div>
+                        <div className="link">
+                            <ul>
+                                <li><h4>SITES</h4></li>
+                                <li><a href="">DC</a></li>
+                                <li><a href="">MAD Magazine</a></li>
+                                <li><a href="">DC Kids</a></li>
+                                <li><a href="">DC Universe</a></li>
+                                <li><a href="">DC Power Visa</a></li>
+                            </ul>
+                        </div>
+
+
+                    </div>
+                    <div className="dc_bg">
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </footer>
+    )
+}
